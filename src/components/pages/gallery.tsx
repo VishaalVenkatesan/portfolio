0528fc64b26fb2e9826b33f9a1307514b5eb6300
Vishaal Venkatesan <vishaalvo3@gmail.com>
2024-09-19 19:06:01 +0530

    gallery title made to start instead of center

--- src/components/pages/gallery.tsx
+++ src/components/pages/gallery.tsx
@@ -32,7 +32,7 @@ export default function Gallery(): JSX.Element {
       <SectionContainer>
         <div className="container mx-auto px-4 py-16">
           <motion.h1 
-            className="text-2xl font-semibold mb-8 text-center"
+            className="text-2xl font-semibold mb-8 text-start"
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
