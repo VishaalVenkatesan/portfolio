449af1181f5f597ac73580402e51ec63cd371e21
Vishaal Venkatesan <vishaalvo3@gmail.com>
2024-09-28 14:40:35 +0530

    vercel analytics enabled

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Footer from "@/components/global/Footer"
 import { AuthContextProvider } from "@/firebase/auth-context";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const [client] = React.useState(new QueryClient())
@@ -17,10 +18,12 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
       <AuthContextProvider>
       <QueryClientProvider client={client}>
         {children}
+        <Analytics/>
         </QueryClientProvider>
         <Toaster />
         <Footer />
         </AuthContextProvider>
     </ThemeProvider>
+    
   );
 }
