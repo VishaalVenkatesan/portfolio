866ede556dec478dd8c1591955bddd989d5d8c55
neoperkcloud <neoperkcloud@gmail.com>
2024-11-07 22:45:27 +0530

    meta data added to the home route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ import Projects from "@/components/pages/projects";
 import Gallery from "@/components/pages/gallery";
 import Contact from "@/components/pages/contact";
 import Blog from "@/components/pages/blog";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Vishaal Venkatesan's Portfolio",
+  description: "The personal portfolio of Vishaal Venkatesan, a developer based in Bengaluru.",
+};
 
 export default function Page(){
   return(
